99037d04a8edd358a7c4aa6806f15118fac7f323
Ben Baker-Smith <bbakersmith@gmail.com>
2014-04-01 21:13:22 -0500

    Fix test layout
    - remove references to posts in index.html
    - create separate layout for pages so index.html renders properly
    - add test for index.html
    - add test for page content

--- spec/pages_dir_spec.rb
+++ spec/pages_dir_spec.rb
@@ -6,15 +6,19 @@ describe '_Pages Directory Generator' do
   it 'should create all the write files in the right places' do
     visit '/level-zero.html'
     page.should have_css 'span#level-name', :text => 'Zero'
+    page.should have_css 'h1#title', :text => 'Generic Page Title'
 
     visit '/level-one/index.html'
     page.should have_css 'span#level-name', :text => 'One'
+    page.should have_css 'h1#title', :text => 'Generic Page Title'
 
     visit '/level-one/level-two/index.html'
     page.should have_css 'span#level-name', :text => 'Two'
+    page.should have_css 'h1#title', :text => 'Generic Page Title'
 
     visit '/level-one/level-two/top-level.html'
     page.should have_css 'span#level-name', :text => 'Top'
+    page.should have_css 'h1#title', :text => 'Generic Page Title'
   end
 
   it 'should still properly handle page content' do
@@ -30,6 +34,16 @@ describe '_Pages Directory Generator' do
 
     visit '/_pages/level-zero.html'
     page.status_code.should == 404
+  end
+
+  it 'should include the zero item test content' do
+    visit '/level-zero.html'
+    page.should have_css 'p.zero-test', :text => 'success'
+  end
+
+  it 'should render index.php properly' do
+    visit '/index.html'
+    page.should have_css 'div#home-test', :text => 'home success'
   end
 
 end
